aa8f8f58e42e78fe8b3f0a3ca7a79659a807a715
br1an17 <brianzairpaez@gmail.com>
2025-06-06 21:16:47 -0300

    normal

--- clase 4/ejercicio.js
+++ clase 4/ejercicio.js
@@ -1,9 +1,9 @@
 const autos = [
-  { marca: "chevrolet", modelo: "prisma", anio: 2019, color: "gris - perlado" },
+  { marca: "chevrolet", modelo: "prisma", anio: 2019, color: "gris-perlado" },
   { marca: "chevrolet", modelo: "sport", anio: 2011, color: "gris" },
   { marca: "chevrolet", modelo: "aveo", anio: 2014, color: "azul" },
   { marca: "renault", modelo: "Clio", anio: 2016, color: "negro" },
-  { marca: "renault", modelo: "logan", anio: 2013, color: "azul - noche" },
+  { marca: "renault", modelo: "logan", anio: 2013, color: "azul-noche" },
   { marca: "wolfvagen", modelo: "amarok", anio: 2010, color: "negro" },
   { marca: "wolfvagen", modelo: "golf", anio: 2024, color: "rojo" },
   { marca: "ferrary", modelo: "sport-611", anio: 1996, color: "rojo" },
